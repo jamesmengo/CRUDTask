9972c79104fdeab3bcea8994d78fb591b3357a3d
James <jamesmeng98@gmail.com>
2020-04-19 17:43:48 -0700

    Initalize Project w/ CR endpoints

---
+++ src/db/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://James:admin@cluster0-wnava.mongodb.net/Tasks?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }
+);
