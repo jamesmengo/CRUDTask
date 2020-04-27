ec6a7d5292573b4c564a203dd919bae0a5c1d9da
James <jamesmeng98@gmail.com>
2020-04-27 15:03:32 -0700

    squash! Add User Avatar and Delete User Avatar

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,141 +1,156 @@
-const express = require('express')
-const router = new express.Router()
-const User = require('../models/user')
-const auth = require('../middleware/auth')
-const multer = require('multer')
+const express = require("express");
+const router = new express.Router();
+const User = require("../models/user");
+const auth = require("../middleware/auth");
+const multer = require("multer");
+const sharp = require("sharp");
 
 // CREATE Endpoints
 router.post("/users", async (req, res) => {
-  const user = new User(req.body)
+  const user = new User(req.body);
   try {
-    await user.save()
-    const token = await user.generateAuthToken()
+    await user.save();
+    const token = await user.generateAuthToken();
     res.status(201).send({
       user,
-      token
-    })
+      token,
+    });
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).send(err);
   }
 });
 
-router.post('/users/login', async (req, res) => {
+router.post("/users/login", async (req, res) => {
   try {
-    const user = await User.findByCredentials(req.body.email, req.body.password)
-    const token = await user.generateAuthToken()
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
     res.send({
       user,
-      token
-    })
+      token,
+    });
   } catch (err) {
-    res.status(400).send(err)
+    res.status(400).send(err);
   }
-})
+});
 
-router.post('/users/logout', auth, async (req, res) => {
+router.post("/users/logout", auth, async (req, res) => {
   try {
     req.user.tokens = req.user.tokens.filter((token) => {
-      return token.token !== req.token
-    })
-    await req.user.save()
-    res.send('User logged out')
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send("User logged out");
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
-})
+});
 
-router.post('/users/logoutAll', auth, async (req, res) => {
+router.post("/users/logoutAll", auth, async (req, res) => {
   try {
-    req.user.token = []
-    await req.user.save()
-    res.send('User logged out of all devices')
+    req.user.token = [];
+    await req.user.save();
+    res.send("User logged out of all devices");
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
-})
+});
 
 // READ Endpoints
 router.get("/users/me", auth, async (req, res) => {
-  res.send(req.user)
-})
+  res.send(req.user);
+});
 
 // UPDATE Endpoints
-router.patch('/users/me', auth, async (req, res) => {
-  const updates = Object.keys(req.body)
-  const allowedUpdates = ['name', 'email', 'password', 'age']
-  const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+router.patch("/users/me", auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidUpdate = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
   if (!isValidUpdate) {
-    return res.status(400).send('Invalid Update')
+    return res.status(400).send("Invalid Update");
   }
   try {
-    const user = req.user
-    updates.forEach((key) => user[key] = req.body[key])
-    await user.save()
+    const user = req.user;
+    updates.forEach((key) => (user[key] = req.body[key]));
+    await user.save();
     if (!user) {
-      return res.status(404).send('Could not locate a user with that ID')
+      return res.status(404).send("Could not locate a user with that ID");
     }
-    res.send(user)
+    res.send(user);
   } catch (err) {
-    return res.status(500).send(err)
+    return res.status(500).send(err);
   }
-})
+});
 
 // DELETE Endpoints
-router.delete('/users/me', auth, async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
   try {
-    await req.user.remove()
-    res.send()
+    await req.user.remove();
+    res.send();
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
-})
+});
 
 // AVATAR Endpoints
 const upload = multer({
   limits: {
-    fileSize: 1000000
+    fileSize: 1000000,
   },
   fileFilter(req, file, cb) {
     if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error('Please upload an image in JPEG or PNG'))
+      return cb(new Error("Please upload an image in JPEG or PNG"));
     }
-    cb(undefined, true)
+    cb(undefined, true);
+  },
+});
+
+router.post(
+  "/users/me/avatar",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({
+      error: error.message,
+    });
   }
-})
+);
 
-router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-  req.user.avatar = req.file.buffer
-  await req.user.save()
-  res.send()
-}, (error, req, res, next) => {
-  res.status(400).send({
-    error: error.message
-  })
-})
-
-router.get('/users/:id/avatar', async (req, res) => {
+router.get("/users/:id/avatar", async (req, res) => {
   try {
-    const user = await User.findById(req.params.id)
+    const user = await User.findById(req.params.id);
     if (!user || !user.avatar) {
-      throw new Error('User or Avatar not found')
+      throw new Error("User or Avatar not found");
     }
 
-    res.set('Content-Type', 'image/jpg')
-    res.send(user.avatar)
+    res.set("Content-Type", "image/jpg");
+    res.send(user.avatar);
   } catch (e) {
-    res.status(404).send(e)
+    res.status(404).send(e);
   }
-})
+});
 
-router.delete('/users/me/avatar', auth, async (req, res) => {
+router.delete("/users/me/avatar", auth, async (req, res) => {
   try {
-    req.user.avatar = undefined
-    await req.user.save()
-    res.send()
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
   } catch (err) {
-    res.status(500).send(err)
+    res.status(500).send(err);
   }
-})
+});
 
-
-module.exports = router
+module.exports = router;
